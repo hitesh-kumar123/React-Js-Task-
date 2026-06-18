7cb3b993439b8e0cda3fcb3f99a8be4dfc7a3346
hitesh-kumar123 <hiteshdevkumar2003@gmail.com>
2026-06-18 10:31:53 +0530

    In store.js create redux store

---
+++ Task6/project-management-app/src/app/store.js
@@ -0,0 +1,44 @@
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+
+import authReducer from "../redux/authSlice";
+import projectReducer from "../redux/projectSlice";
+import boardReducer from "../redux/boardSlice";
+import taskReducer from "../redux/taskSlice";
+import userReducer from "../redux/userSlice";
+import notificationReducer from "../redux/notificationSlice";
+
+import storage from "redux-persist/lib/storage";
+
+import {
+  persistReducer,
+  persistStore,
+} from "redux-persist";
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+  projects: projectReducer,
+  boards: boardReducer,
+  tasks: taskReducer,
+  users: userReducer,
+  notifications: notificationReducer,
+});
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(
+  persistConfig,
+  rootReducer
+);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+
+export const persistor = persistStore(store);
